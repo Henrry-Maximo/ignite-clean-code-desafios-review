5f6e29bfe760b74911d76a0f13c5d67b7fc581b0
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-01-06 13:51:41 -0300

    ✨Feat: Add variable time and names with format

--- desafios/07-numeros-magicos.js
+++ desafios/07-numeros-magicos.js
@@ -1,5 +1,11 @@
 function lookForUpdades() {}
 
-setInterval(lookForUpdades, 60 * 30 * 1000)
+const interval_30_minutes = 1000 * 60 * 30;
 
-function calculateDiscount(price, discount) {}
+setInterval(lookForUpdades, 60 * 30 * 1000) // 30 minutes
+
+// what is more format of data
+// price is number integer at database (convert of float for int)
+function calculateDiscount(priceAtCents, discountAmountAtPorcentage) {}
+
+// 1_000_000
