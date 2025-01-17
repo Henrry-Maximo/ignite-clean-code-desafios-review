cdc5393a196951fbf4647b163985360d07110d48
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-01-17 13:52:52 -0300

    📚 Refactor: Remove comments and add explication function convert jpg

--- desafios/08-comentarios-vs-documentacao.js
+++ desafios/08-comentarios-vs-documentacao.js
@@ -1,28 +1,23 @@
-// Essa função tem o objetivo de registrar um novo usuário no banco.
-async function register(data) {
-  // Desestrutura os dados recebidos.
-  const { email, name, avatar } = data
+ 
+/* 
+  Essa função `convertImageToJPG` realiza a conversão das imagens para JPG a fim de evitar erros de incompatibilidade.
+  Mais informações na issue https://github.com/rocketseat-education/example-repository/issues/1
+*/
 
-  // Verifica se o avatar foi informado.
-  if (!avatar) return { error: 'avatar is required' }
+async function registerUserInDatabase(data) {
+  const { email, name, avatar } = data;
 
-  // Verifica se o nome foi informado.
-  if(!name) return { error: 'name is required' }
+  if (!avatar || !email || !name) return { error: 'avatar or email or name is required' }
 
-  // Essa função retorna o usuário a partir do email informado
-  const userMail = getUserByEmail(email)
+  const userMail = getUserByEmail(email);
 
-  // Verificação se o email informado já foi utilizado por algum usuário.
   if (userMail) {
     return { error: 'email already used' }
   }
 
-  // Essa função realiza a conversão das imagens para JPG a fim de evitar erros de incompatibilidade.
-  // Mais informações na issue https://github.com/rocketseat-education/example-repository/issues/1
-  const avatar2 = convertImageToJPG(avatar)
+  const avatarInFormattingJPG = convertImageToJPG(avatar);
 
-  // Cria o usuário no banco de dados
-  const user = await createUser({ email, name, avatar: avatar2 })
+  const user = await createUser({ email, name, avatar: avatarInFormattingJPG })
 
   return { user }
 }
